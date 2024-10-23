c2aeb6e060e8147030e06ce291f95b0334eda9db
iwerxs <iworxs@gmail.com>
2024-10-23 20:12:13 +0100

    update avatar in ui

---
+++ src/app/(main)/users/[username]/actions.ts
@@ -0,0 +1,26 @@
+//src/app/(main)/users/[username]/actions.ts
+//actions for the user profile
+"use server";
+
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getUserDataSelect } from "@/lib/types";
+import {
+  updateUserProfileSchema,
+  UpdateUserProfileValues,
+} from "@/lib/validation";
+
+export async function updateUserProfile(values: UpdateUserProfileValues) {
+  const validateValues = updateUserProfileSchema.parse(values);
+
+  const { user } = await validateRequest();
+
+  if (!user) throw new Error("Unauthorized");
+
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: validateValues,
+    select: getUserDataSelect(user.id),
+  });
+  return updatedUser;
+}
