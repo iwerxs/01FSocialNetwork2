9c67faaacc297ca9e664d7875f6ee6c833c99437
iwerxs <iworxs@gmail.com>
2024-10-24 15:32:05 +0100

    updated static navbar position

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="sticky top-8 z-10 bg-card shadow-sm">
+    <header className="sticky top-1 z-10 bg-card shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-center gap-5 px-5 py-3">
         <Link href="/" className="text-2xl font-bold text-primary">
           Social Network
