04e42a24b8360b03f1d2562bafab7ca13bce3c22
iwerxs <iworxs@gmail.com>
2024-11-05 09:50:29 +0000

    bookmarks for main page ui

---
+++ src/app/(main)/bookmarks/Bookmarks.tsx
@@ -0,0 +1,67 @@
+//src/app/(main)/bookmarks/Bookmarks.tsx
+//React Query Client component for the route.ts file in the 'bookmarks' server folder
+"use client";
+
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import DeletePostDialog from "@/components/posts/DeletePostDialog";
+import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import { Button } from "@/components/ui/button";
+import kyInstance from "@/lib/ky";
+import { PostData, PostsPage } from "@/lib/types";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+export default function Bookmarks() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "bookmarks"],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/bookmarked",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor || null,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  // first page is loading
+  if (status === "pending") {
+    return <PostsLoadingSkeleton />;
+  }
+  // check if feed is empty
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">No bookmarks yet</p>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">Error loading bookmarks</p>
+    );
+  }
+
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+      <DeletePostDialog open={false} onClose={() => {}} post={posts[0]} />
+    </InfiniteScrollContainer>
+  );
+}
