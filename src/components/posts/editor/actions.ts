04c47358b199fc53ed00dc93bd7a475ba4ea7f7f
iwerxs <iworxs@gmail.com>
2024-11-06 17:14:31 +0000

    posts editor component updates

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -1,4 +1,33 @@
 //src/components/posts/editor/actions.ts
 //server actions for posts
+"use server";
 
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getPostDataInclude } from "@/lib/types";
+import { createPostSchema } from "@/lib/validation";
+
+export async function submitPost(input: {
+  content: string;
+  mediaIds: string[];
+}) {
+  const { user } = await validateRequest();
+
+  if (!user) throw new Error("Unauthorized");
+
+  const { content, mediaIds } = createPostSchema.parse(input);
+
+  const newPost = await prisma.post.create({
+    data: {
+      content,
+      userId: user.id,
+      attachments: {
+        connect: mediaIds.map((id) => ({ id })),
+      },
+    },
+    include: getPostDataInclude(user.id),
+  });
+
+  return newPost;
+}
 // check user is authenticated
