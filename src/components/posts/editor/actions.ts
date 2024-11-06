ad9d6478ca119d5a554efa68338de763c246ab9d
iwerxs <iworxs@gmail.com>
2024-11-06 17:12:03 +0000

    post editor updated

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -1,34 +1,4 @@
 //src/components/posts/editor/actions.ts
 //server actions for posts
-//always authenticate via the backend, never the frontend, as this can be manipulated.
-"use server";
-
-import { validateRequest } from "@/auth";
-import prisma from "@/lib/prisma";
-import { getPostDataInclude } from "@/lib/types";
-import { createPostSchema } from "@/lib/validation";
 
 // check user is authenticated
-export async function submitPost(input: {
-  content: string;
-  mediaIds: string[];
-}) {
-  const { user } = await validateRequest();
-
-  if (!user) throw new Error("Unauthorized");
-
-  const { content, mediaIds } = createPostSchema.parse(input);
-
-  const newPost = await prisma.post.create({
-    data: {
-      content,
-      userId: user.id,
-      attachments: {
-        connect: mediaIds.map((id) => ({ id })),
-      },
-    },
-    include: getPostDataInclude(user.id),
-  });
-  // update through React Queries via api, not a 'revalidate path'
-  return newPost;
-}
