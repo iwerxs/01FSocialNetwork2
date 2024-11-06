4d0da87097428cdcb747011c24f3b364831beb87
iwerxs <iworxs@gmail.com>
2024-11-06 16:22:37 +0000

    updated main components

--- src/components/InfiniteScrollContainer.tsx
+++ src/components/InfiniteScrollContainer.tsx
@@ -1,2 +1,30 @@
 //src/components/InfiniteScrollContainer.tsx
 //trigger for next page
+import { useInView } from "react-intersection-observer";
+
+interface InfiniteScrollContainerProps extends React.PropsWithChildren {
+  onBottomReached: () => void;
+  className?: string;
+}
+
+export default function InfiniteScrollContainer({
+  children,
+  onBottomReached,
+  className,
+}: InfiniteScrollContainerProps) {
+  const { ref } = useInView({
+    rootMargin: "200px",
+    onChange(inView) {
+      if (inView) {
+        onBottomReached();
+      }
+    },
+  });
+
+  return (
+    <div className={className}>
+      {children}
+      <div ref={ref} />
+    </div>
+  );
+}
