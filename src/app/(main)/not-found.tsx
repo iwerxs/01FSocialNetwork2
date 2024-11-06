e76bf13e1e8941c5658dff859e61fc32173eb4a1
iwerxs <iworxs@gmail.com>
2024-11-06 19:05:42 +0000

    main ui components

--- src/app/(main)/not-found.tsx
+++ src/app/(main)/not-found.tsx
@@ -1,2 +1,10 @@
 //src/app/(main)/not-found.tsx
 //404 page not found
+export default function NotFound() {
+  return (
+    <main className="my-12 w-full space-y-3 text-center">
+      <h1 className="text-3xl font-bold">Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </main>
+  );
+}
