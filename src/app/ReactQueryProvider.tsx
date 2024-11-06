680cd41fc18539bb76131538babac0ee0e00dd44
iwerxs <iworxs@gmail.com>
2024-11-06 17:43:20 +0000

    main app file components

--- src/app/ReactQueryProvider.tsx
+++ src/app/ReactQueryProvider.tsx
@@ -1,2 +1,22 @@
 //src/app/ReactQueryProvider.tsx
 //Context Provider for the {children} component
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [client] = useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
