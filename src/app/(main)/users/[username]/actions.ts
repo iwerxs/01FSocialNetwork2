95d68be76620cad8d1f43103b3c47633124bc9ba
iwerxs <iworxs@gmail.com>
2024-11-06 19:22:38 +0000

    users username components updates

--- src/app/(main)/users/[username]/actions.ts
+++ src/app/(main)/users/[username]/actions.ts
@@ -1,2 +1,37 @@
 //src/app/(main)/users/[username]/actions.ts
 //actions for the user profile
+"use server";
+
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import streamServerClient from "@/lib/stream";
+import { getUserDataSelect } from "@/lib/types";
+import {
+  updateUserProfileSchema,
+  UpdateUserProfileValues,
+} from "@/lib/validation";
+
+export async function updateUserProfile(values: UpdateUserProfileValues) {
+  const validatedValues = updateUserProfileSchema.parse(values);
+
+  const { user } = await validateRequest();
+
+  if (!user) throw new Error("Unauthorized");
+
+  const updatedUser = await prisma.$transaction(async (tx) => {
+    const updatedUser = await tx.user.update({
+      where: { id: user.id },
+      data: validatedValues,
+      select: getUserDataSelect(user.id),
+    });
+    await streamServerClient.partialUpdateUser({
+      id: user.id,
+      set: {
+        name: validatedValues.displayName,
+      },
+    });
+    return updatedUser;
+  });
+
+  return updatedUser;
+}
