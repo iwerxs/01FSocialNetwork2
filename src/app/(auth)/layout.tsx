39ad3466f8c5b3325f12ab73b343abfb72ae4548
iwerxs <iworxs@gmail.com>
2024-11-07 08:38:06 +0000

    logout functions

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,1 @@
 //src/app/(auth)/layout.tsx
-//Layout Component for the login and signup pages
-
-import { validateRequest } from "@/auth";
-import { redirect } from "next/navigation";
-
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const { user } = await validateRequest();
-
-  if (user) redirect("/");
-
-  return <>{children}</>;
-}
