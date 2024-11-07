87e9050b185473822a93cc07f91763069dbc6fda
iwerxs <iworxs@gmail.com>
2024-11-07 14:04:37 +0000

    updated

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -16,10 +16,10 @@ export default function Page() {
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
           <div className="space-y-1 text-center">
-            <h1 className="text-3xl font-bold">Sign up to bugbook</h1>
+            <h1 className="text-3xl font-bold">Sign Up to Social Network</h1>
             <p className="text-muted-foreground">
-              A place where even <span className="italic">you</span> can find a
-              friend.
+              A place where <span className="italic">you</span> can find
+              newtwork with friends.
             </p>
           </div>
           <div className="space-y-5">
